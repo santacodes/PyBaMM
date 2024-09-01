0ad05fe72c9c8270fc99bfc7e4c167898b5b04b4
Santhosh Sundaram <santhoshsundaram9650@gmail.com>
2024-09-02 00:27:30 +0530

    added hatch as a build system

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-
-# Project metadata was moved to pyproject.toml (which is read by pip). However, custom
-# build commands and setuptools extension modules are still defined here.
-setup(
-    # silence "Package would be ignored" warnings
-    include_package_data=True,
-)
